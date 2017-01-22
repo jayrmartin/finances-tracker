420a76c5f2b5092165370b532b40ad5121c7cdc3
jayrmartin <jrmartin@alumni.uwaterloo.ca>
2017-01-22 12:16:37 -0500

    Adding outlets for UI elements to main view controller. Adding TransactionDetailsViewController class which will handle adding or editing transaction details.

--- Finances-Tracker/Finances-Tracker/ViewController.swift
+++ Finances-Tracker/Finances-Tracker/ViewController.swift
@@ -8,20 +8,52 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
+class ViewController: NSViewController
+{
+    // References to specific UI objects
+    @IBOutlet weak var newButton: NSButton!
+    @IBOutlet weak var loadButton: NSButton!
+    @IBOutlet weak var addButton: NSButton!
+    @IBOutlet weak var transactionsTableView: NSTableView!
+    
+    // List of existing transactions
+    fileprivate var transactions: [NSManagedObject] = []
+    
+    // Separate view controller to add or edit transaction details
+    fileprivate lazy var transactionDetailsViewController: TransactionDetailsViewController = {
+        return self.storyboard!.instantiateController(withIdentifier: "TransactionDetailsViewController") as! TransactionDetailsViewController
+    }()
+    
+    @IBAction func handleNewButtonPress(_ sender: Any)
+    {
+        print("New Button Pressed!")
+    }
+    
+    @IBAction func handleLoadButtonPress(_ sender: Any)
+    {
+        print("Load Button Pressed")
+    }
+    
+    @IBAction func handleAddButtonPress(_ sender: Any)
+    {
+        print("Add Button Pressed")
+        
+        //self.presentViewControllerAsSheet(transactionDetailsViewController)
+        self.presentViewControllerAsModalWindow(transactionDetailsViewController)
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
+    override var representedObject: Any?
+    {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
